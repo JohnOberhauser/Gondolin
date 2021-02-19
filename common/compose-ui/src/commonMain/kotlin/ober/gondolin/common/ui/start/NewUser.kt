843b9655ace412242a942e8a7dbb517becff41c8
John Oberhauser <john.oberhauser@live.com>
2021-02-18 20:50:48 -0600

    saving

--- common/compose-ui/src/commonMain/kotlin/ober/gondolin/common/ui/start/NewUser.kt
+++ common/compose-ui/src/commonMain/kotlin/ober/gondolin/common/ui/start/NewUser.kt
@@ -10,10 +10,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Dp
 import ober.gondolin.common.ui.common.textStyle.TextStyles
