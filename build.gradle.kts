b7024827118c92fc6ef9509a16bf48d3ae24f413
John Oberhauser <john.oberhauser@live.com>
2021-02-26 00:44:05 -0600

    saving

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 
 allprojects {
     repositories {
-        //mavenCentral()
-        //maven("https://kotlin.bintray.com/kotlinx")
         google()
         jcenter()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
